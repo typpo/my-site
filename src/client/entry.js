0984eb6e4dcf4a88f3bf300b6a3f9d5d57a1e1cf
Ian Webster <ianw_github@ianww.com>
2015-09-29 15:14:07 -0700

    fix js loading

--- src/client/entry.js
+++ src/client/entry.js
@@ -3,5 +3,5 @@ import Router from 'react-router';
 import routes from '../shared/routes';
 
 Router.run(routes, Router.HistoryLocation, (Handler, state) => {
-  React.render(<Handler />, document.getElementById('app'));
+  React.render(<Handler />, document.getElementById('content'));
 });
