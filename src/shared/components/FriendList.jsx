2010ff755faed010be33697684232b5c5a279911
Ian Webster <ianw_git@ianww.com>
2019-05-16 22:20:59 -0700

    add Webring

--- src/shared/components/FriendList.jsx
+++ src/shared/components/FriendList.jsx
@@ -8,9 +8,10 @@ export default class FriendList extends React.Component {
   render() {
     return (
       <div className="post-list generic-list">
-        <h3>Friends</h3>
+        <h3>Join my webring!</h3>
         <ul>
-        <li><a href="https://leafo.net/">Leaf</a> - Programmer</li>
+          <li><a href="http://www.judymou.com/">Judy</a> - Senior Software Engineer</li>
+          <li><a href="mail:webring@ianww.com">+ you?</a></li>
         </ul>
       </div>
     );
