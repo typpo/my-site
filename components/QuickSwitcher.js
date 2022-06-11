174a06eb19447d9d0ec75bd92aa121983f45776c
Ian Webster <ianw_github@ianww.com>
2022-06-10 19:08:30 -0700

    quickswitcher css

--- components/QuickSwitcher.js
+++ components/QuickSwitcher.js
@@ -1,19 +1,19 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const QuickSwitcher = () => {
+  const router = useRouter();
+
   return (
-    <div>
-      <div className="nav">
-        <strong>&raquo;</strong> &nbsp;
-        <Link href="/" activeClassName="active">
-          <a>Projects</a>
-        </Link>
-        &nbsp;&middot;&nbsp;
-        <Link href="/talks" activeClassName="active">
-          <a>Talks</a>
-        </Link>
-      </div>
-      <div></div>
+    <div className="nav">
+      <strong>&raquo;</strong> &nbsp;
+      <Link href="/">
+        <a className={router.pathname === '/' ? 'active' : ''}>Projects</a>
+      </Link>
+      &nbsp;&middot;&nbsp;
+      <Link href="/talks">
+        <a className={router.pathname === '/talks' ? 'active' : ''}>Talks</a>
+      </Link>
     </div>
   );
 };
