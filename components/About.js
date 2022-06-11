969a0cf0507e7ad39cc3e3e843c0bf2958a57f13
mldangelo <michael.l.dangelo@gmail.com>
2022-06-10 18:27:53 -0700

    lint

--- components/About.js
+++ components/About.js
@@ -5,39 +5,39 @@ const About = () => {
           <div id="about">
             <p>
               I co-founded{" "}
-              <a href="http://www.zenysis.com" target="_blank">
+              <a href="http://www.zenysis.com" target="_blank" rel="noreferrer">
                 Zenysis
               </a>{" "}
               and lead its engineering team. Before that I worked at{" "}
-              <a href="http://google.com" target="_blank">
+              <a href="http://google.com" target="_blank" rel="noreferrer">
                 Google
               </a>
               , mostly on travel search (via acquisition of{" "}
-              <a href="http://room77.com" target="_blank">
+              <a href="http://room77.com" target="_blank" rel="noreferrer">
                 Room 77
               </a>
               ).
             </p>
             <p>
               A while ago I created{" "}
-              <a href="http://www.asterank.com" target="_blank">
+              <a href="http://www.asterank.com" target="_blank" rel="noreferrer">
                 Asterank
               </a>
               , which was acquired by{" "}
               <a
                 href="http://www.planetaryresources.com/#home-intro"
-                target="_blank"
+                target="_blank" rel="noreferrer"
               >
                 Planetary Resources
               </a>
               . Afterwards I began consulting via{" "}
               <a href="http://alioth.io">Alioth</a> with a focus on data
               visualization, working with{" "}
-              <a href="https://www.nasa.gov/" target="_blank">
+              <a href="https://www.nasa.gov/" target="_blank" rel="noreferrer">
                 NASA
               </a>
               ,{" "}
-              <a href="http://www.seti.org/" target="_blank">
+              <a href="http://www.seti.org/" target="_blank" rel="noreferrer">
                 SETI
               </a>
               , and various private companies.
--- components/Project.js
+++ components/Project.js
@@ -20,7 +20,7 @@ const Project = ({data, isPriority }) => {
   }
 
   return (
-    <a className="project-link" target="_blank" href={data.url}>
+    <a className="project-link" target="_blank" href={data.url} rel="noreferrer">
       <div className="project flex-item-default">
         <Image src={data.imgurl} layout="fill" priority={data.isPriority}/>
         <h3 className="title">{data.title}</h3>
