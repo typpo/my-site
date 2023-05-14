6bb05c4fcd4068b6a00a844793dd44e234f89097
Ian Webster <ianw_github@ianww.com>
2023-05-14 12:52:32 -0700

    set up favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+import Favicon from '../components/Favicon';
+
 const Document = () => {
   return (
     <Html>
       <Head>
+        <Favicon />
         <link
           href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900&display=optional"
           rel="stylesheet"
